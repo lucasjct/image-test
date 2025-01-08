4aa299dcaf462fba22ea420fb4a5f2a51b75b416
Lucas Teixeira <lucasjcteixeira@gmail.com>
2025-01-08 10:02:21 -0300

    added context and when from ginkgo

--- tests/node_image_test.go
+++ tests/node_image_test.go
@@ -7,19 +7,25 @@ import (
 )
 
 var _ = Describe("Check node version from dockerfile", func() {
-	It("should match the version", func() {
+	var (
+		tag          string
+		buildOptions *docker.BuildOptions
+	)
 
-		tag := "undistrotest/docker_node_alpine_test"
-		buildOptions := &docker.BuildOptions{
+	BeforeEach(func() {
+		tag = "undistrotest/docker_node_alpine_test"
+		buildOptions = &docker.BuildOptions{
 			Tags: []string{tag},
 		}
 
 		docker.Build(GinkgoT(), "../images/docker_node_alpine", buildOptions)
-
-		opts := &docker.RunOptions{Command: []string{"node", "--version"}}
-		output := docker.Run(GinkgoT(), tag, opts)
-		//assert.Equal(GinkgoT(), "v20.18.1", output)
-		Expect("v20.18.1").To(Equal(output))
 	})
 
+	When("running the container", func() {
+		It("should match the version", func() {
+			opts := &docker.RunOptions{Command: []string{"node", "--version"}}
+			output := docker.Run(GinkgoT(), tag, opts)
+			Expect("v20.18.1").To(Equal(output))
+		})
+	})
 })
--- tests/python_image_test.go
+++ tests/python_image_test.go
@@ -7,20 +7,27 @@ import (
 )
 
 var _ = Describe("Check network connection with Python image", func() {
-	It("should return 200", func() {
-		GinkgoT().Parallel()
 
-		tag := "undistrotest/docker_python_test"
-		buildOptions := &docker.BuildOptions{
+	var (
+		tag          string
+		buildOptions *docker.BuildOptions
+	)
+
+	BeforeEach(func() {
+		tag = "undistrotest/docker_python_test"
+		buildOptions = &docker.BuildOptions{
 			Tags: []string{tag},
 		}
 
 		docker.Build(GinkgoT(), "../images/docker_python", buildOptions)
-
-		opts := &docker.RunOptions{Command: []string{"python", "send_request.py"}}
-		output := docker.Run(GinkgoT(), tag, opts)
-		// assert.Equal(GinkgoT(), "200", output)
-		Expect("200").To(Equal(output))
 	})
 
+	When("running the container", func() {
+		It("should return 200", func() {
+			opts := &docker.RunOptions{Command: []string{"python", "send_request.py"}}
+			output := docker.Run(GinkgoT(), tag, opts)
+			Expect("200").To(Equal(output))
+		})
+
+	})
 })
